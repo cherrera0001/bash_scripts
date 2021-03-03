e379e44a6a05b2a5f8b2e0b4d118ecd531f49daf
cherrera0001 <herrera.jara.cristobal@gmail.com>
2021-03-03 16:51:19 -0300

    Update portScan.sh

--- portScan.sh
+++ portScan.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# ./portScan.sh  <ip-address>
+if [$1]; then
+       ip_address=$1
+       for port i in $(seq 1 65535); do
+               timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port" && echo "El puerto está abierto"
+       done
+else
+       echo -e "\[*] Uso: ./portScan.sh  <ip-address>\n"
+       exit 1
+fi
